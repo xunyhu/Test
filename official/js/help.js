4241efc9c03e10f5e24bcab8c889ea5647f56ce7
HuRui <xunyihu@163.com>
2018-01-04 16:25:12 +0800

    modified help

--- official/js/help.js
+++ official/js/help.js
@@ -1,11 +1,4 @@
 $(function(){
     //切换主题
     var $li = $('#m-content .left li');
-    $.each($li, function(index, ele) {
-        ele.click(function(){
-            $li.removeClass('active');
-            $(this).addClass('active');
-            $('#m-content .right div').removeClass('active').eq(index).addClass('active');
-        })
-    });
 })
